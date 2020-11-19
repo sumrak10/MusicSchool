b71ba9b1e908ccd7a19baba9aef612ce4c441aee
sumrak10 <58951184+sumrak10@users.noreply.github.com>
2020-11-19 17:04:05 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function sendMail() {
+    var link = "mailto:nurlan.ulov@gmail.com" //кому
+             + "?cc=nurlan.urazkulov@gmail.com" //от кого
+             + "&subject=" + encodeURIComponent("This is my subject") //тема
+             + "&body=" + encodeURIComponent(document.getElementById('myText').value); //текст
+    window.location.href = link;
+}
+
+document.forms.reg.onsubmit = function() {
+    var name = this.name.value;
+    var contact = this.contact.value;
+    var email = this.email.value;
+    var instrument = this.instrument.value;
+    var link = "mailto:nurlan.ulov@gmail.com" //кому
+             + "?cc=" + email //от кого
+             + "&subject=" + "Новая заявка!" //тема
+             + "&body=" + "Имя и фамилия:" + name + "  Контакты" + contact + "  Иструмент:" + instrument; //текст
+    window.location.href = link;
+    return false;
+};
