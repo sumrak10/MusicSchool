3425f43c73144ec59d3f48c7f7aa682c4a738998
unknown <nurlan.urazkulov@gmail.com>
2020-12-10 16:43:37 +0500

    responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementsByClassName('burger')[0].addEventListener("click", (e) => {
+        if (document.getElementsByClassName('menu')[0].style.display == "flex"){
+            document.getElementsByClassName('burger')[0].classList.remove("burger-active")
+            document.getElementsByClassName('menu')[0].style.display = "none"
+            document.getElementsByClassName('menu')[0].classList.remove("resp-menu")
+        }
+        else {
+            document.getElementsByClassName('burger')[0].classList.add("burger-active")
+            document.getElementsByClassName('menu')[0].style.display = "flex"
+            document.getElementsByClassName('menu')[0].classList.add("resp-menu")
+        }
+    })
+})
+
 function sendMail() {
     var link = "mailto:babaevi@mail.ru" //кому
              + "?cc=nurlan.urazkulov@gmail.com" //от кого
